5b93f0d7ff8b0af29d29885501f10c7ee7177064
João Oliveira <joaooliveira1999@outlook.com>
2024-08-25 23:06:38 -0300

    feat: added tests the content

--- website/src/pages/HomePage.tsx
+++ website/src/pages/HomePage.tsx
@@ -1,5 +1,10 @@
+import { Projects } from "../components/contents/project1";
 import { MainLayout } from "../layout/MainLayout";
 
 export const HomePage = () => {
-  return <MainLayout>test</MainLayout>;
+  return (
+    <MainLayout>
+      <Projects />
+    </MainLayout>
+  );
 };
